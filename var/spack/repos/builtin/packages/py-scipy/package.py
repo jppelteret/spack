82454d7c1a6d9e27874f0e986a159b46a058a702
Jean-Paul Pelteret <jppelteret@gmail.com>
2016-07-19 09:49:08 +0200

    Fixed some python packages

--- var/spack/repos/builtin/packages/py-scipy/package.py
+++ var/spack/repos/builtin/packages/py-scipy/package.py
@@ -32,9 +32,14 @@ class PyScipy(Package):
     version('0.17.0', '5ff2971e1ce90e762c59d2cd84837224')
     version('0.15.1', 'be56cd8e60591d6332aac792a5880110')
     version('0.15.0', '639112f077f0aeb6d80718dc5019dc7a')
+    
+    variant('atlas', default=False,  description='Compile with ATLAS')
 
     extends('python')
     depends_on('py-nose', type='build')
+    depends_on('atlas',   when='+atlas')
+    depends_on('blas',    when='~atlas')
+    depends_on('lapack',  when='~atlas')
     depends_on('py-numpy+blas+lapack', type=nolink)
 
     def install(self, spec, prefix):
